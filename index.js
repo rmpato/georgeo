9139958cd78282c177a703c01a824ec5005a2d79
Patricio Martin Rodriguez <mrodriguez@plataforma.com>
2017-05-02 16:21:49 -0300

    Initial interface to geocode. Initial geocode functionality with OpenStreetMap.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const express = require('express');
+const app = express();
+
 const NodeGeocoder = require('node-geocoder');
 
 let options = { provider: 'openstreetmap' };
 
 let geocoder = NodeGeocoder(options);
 
-geocoder.geocode('Avenida Rivadavia 4200, Buenos Aires, Argentina')
+geocoder.geocode('Buenos Aires, Argentina')
     .then(res => console.log(res))
     .catch(err => console.log(err));
 
+app.use(express.static('web'));
+
+app.listen(3000);
